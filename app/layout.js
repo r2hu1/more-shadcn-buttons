eb3797509bc24ff1b23541b8f5289e3958238a90
r2hu1 <newasram86@gmail.com>
2024-07-08 20:04:56 +0530

    alm dn

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -11,7 +12,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="absolute top-0 z-[-2] h-screen w-screen dark:bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
